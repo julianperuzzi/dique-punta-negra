fec397ac7ea5f8d544b1126dd103c88a4c2e1061
Julian Peruzzi <peru964@gmail.com>
2024-08-05 17:00:51 -0300

    Update PresentationScreen.jsx

--- src/components/PresentationScreen.jsx
+++ src/components/PresentationScreen.jsx
@@ -14,7 +14,7 @@ function PresentationScreen() {
       <div className="text-left md:text-center text-white p-10">
         <h1 className="text-5xl md:text-7xl font-bold mb-4 md:pb-6 " data-aos="fade-up">Bienvenidos al Dique<span className='text-pink-600'> Punta Negra</span></h1>
         <p className="text-lg md:text-2xl border-l-4 border-pink-600 pl-4 md:pl-0 md:border-l-0 md:border-t-2 md:pt-4" data-aos="fade-up" data-aos-delay="200">
-          Descubre un lugar espectacular en San Juan, Argentina. Explora nuestras actividades y servicios.
+          Descubre un lugar único en Zonda, San Juan, Argentina. Explora sus actividades y servicios.
         </p>
       </div>
       </section>
