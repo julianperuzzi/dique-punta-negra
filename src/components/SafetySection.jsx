0d95a868820a5ec7b16baa4298fd9d280edcd8e5
Julian Peruzzi <peru964@gmail.com>
2024-08-05 17:28:24 -0300

    Update SafetySection.jsx

--- src/components/SafetySection.jsx
+++ src/components/SafetySection.jsx
@@ -7,11 +7,11 @@ function SafetySection() {
   return (
     <section className="">
         <div className="bg-white/95 dark:bg-slate-800  dark:bg-opacity-95 backdrop-blur-sm min-h-screen flex items-center justify-center flex-col py-24">
-        <h2 className="text-4xl font-bold md:mb-6 text-center text-white bg-rose-500 w-full p-4 uppercase">Seguridad en el Dique</h2>
+        <h2 className="text-4xl font-bold md:mb-6 text-center text-rose-500 dark:text-white w-full py-6 uppercase">Seguridad en el Dique</h2>
 
           <div className="flex flex-col md:flex-row-reverse items-center md:mx-8 gap-6 justify-center text-center md:text-left">
             
-            <div className="md:w-1/2 md:max-h-[70vh] mx-auto ">
+            <div className="md:w-2/5 md:max-h-[70vh] mx-auto ">
               <img
                 src={imgNautica}
                 alt="Seguridad Náutica"
@@ -19,7 +19,7 @@ function SafetySection() {
               />
             </div>
 
-            <div className="mx-auto md:w-1/2 p-4">
+            <div className="mx-auto md:w-3/5 p-4">
               
               <p className="text-lg text-black dark:text-white/90 mb-6">
                 El Dique Punta Negra cuenta con un equipo de <span className="font-semibold text-rose-500 uppercase">Seguridad Náutica</span> dedicado a resguardar la seguridad de las personas tanto dentro como fuera del agua para el disfrute de todos.
