998fec7b5ec95319f3f5279adb11772fe1ef8298
Julian Peruzzi <peru964@gmail.com>
2024-09-16 01:15:51 -0300

    Update MiniBanner.jsx

--- src/components/MiniBanner.jsx
+++ src/components/MiniBanner.jsx
@@ -3,29 +3,29 @@ import { FaCalendarAlt, FaSwimmer, FaUmbrellaBeach, FaUtensils } from 'react-ico
 
 const MiniBanner = () => {
   return (
-    <div className="bg-orange-500 text-white py-4 flex justify-between md:justify-around items-center md:h-[15vh]  text-center space-x-2 px-2" >
+    <div className="bg-teal-600 text-white py-4 flex md:justify-around items-start md:items-center md:h-[10vh] h-[15vh] text-center space-x-4 px-2 pt-6 md:space-x-0" >
       {/* Días del año */}
       <div className="flex flex-col items-center">
-        <FaCalendarAlt size={30} className="mb-2" />
-        <p className="text-sm font-semibold">365 días del año</p>
+        <FaCalendarAlt  className="mb-2 text-2xl md:text-3xl" />
+        <p className="text-sm ">365 días del año</p>
       </div>
 
       {/* Deportes acuáticos */}
       <div className="flex flex-col items-center">
-        <FaSwimmer size={30} className="mb-2" />
-        <p className="text-sm font-semibold">Deportes acuáticos</p>
+        <FaSwimmer  className="mb-2 text-2xl md:text-3xl " />
+        <p className="text-sm ">Deportes acuáticos</p>
       </div>
 
       {/* Turismo */}
       <div className="flex flex-col items-center">
-        <FaUmbrellaBeach size={30} className="mb-2" />
-        <p className="text-sm font-semibold">Turismo</p>
+        <FaUmbrellaBeach  className="mb-2 text-2xl md:text-3xl" />
+        <p className="text-sm ">Turismo</p>
       </div>
 
       {/* Gastronomía */}
       <div className="flex flex-col items-center">
-        <FaUtensils size={30} className="mb-2" />
-        <p className="text-sm font-semibold">Gastronomía</p>
+        <FaUtensils  className="mb-2 text-2xl md:text-3xl" />
+        <p className="text-sm ">Gastronomía</p>
       </div>
     </div>
   );
